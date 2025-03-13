8baa50ee16983606ff27fc8b376b027927c62a11
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-13 13:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+
+const input = document.querySelector("#evaluatedText");
+// const letterCount = document.querySelector("#letterCount");
+
+ const input = document.getElementById("evaluatedText");
+        const letterCount = document.getElementById("letterCount");
+
+        // Update letter count in real-time
+        input.addEventListener("input", () => {
+            letterCount.innerText = input.value.length; // Count letters
+        });
